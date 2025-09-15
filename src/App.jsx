3ae91f3c269cbd2c2bd3a028167e9c1ea2b84d1f
dpgaire <gairhedurga13@gmail.com>
2025-09-15 21:03:39 +0545

    add search feature

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import InstallPWAButton from "./components/InstallPWAButton";
 import SplashScreen from "./components/SplashScreen";
 import TermsOfService from "./pages/TermsOfService";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
+import NotFoundPage from "./pages/NotFoundPage"; // Import the new page
 
 function App() {
   return (
@@ -29,7 +30,7 @@ function App() {
               <Route path="/projects" element={<ProjectPage />} />
               <Route path="/terms-of-service" element={<TermsOfService />} />
               <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-
+              <Route path="*" element={<NotFoundPage />} /> 
             </Routes>
           </Layout>
         </ThemeProvider>
